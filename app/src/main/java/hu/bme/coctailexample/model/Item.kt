1067c91468c18569ea4462198ea47d339e4bc1b7
Ágocsi-Kiss Bence <akbence007@gmail.com>
2019-04-29 01:36:51 +0200

    model refine

--- app/src/main/java/hu/bme/coctailexample/model/Item.kt
+++ app/src/main/java/hu/bme/coctailexample/model/Item.kt
@@ -3,8 +3,12 @@ package hu.bme.coctailexample.model
 import com.google.gson.annotations.SerializedName
 
 data class Item(
+    @SerializedName("idDrink")
+    var id: String? = null,
     @SerializedName("strDrink")
-    var href: String? = null,
-    @SerializedName("strAlcoholic")
-    var items: String? = null
+    var name: String? = null,
+    @SerializedName("strInstructions")
+    var instructions: String? = null,
+    @SerializedName("strDrinkThumb")
+    var url: String? = null
 )
